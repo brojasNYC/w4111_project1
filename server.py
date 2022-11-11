d9f8cac2560e42f84cdbf42c59998dcff113a4a7
Brandon C. Rojas <brojas3@binghamton.edu>
2022-11-10 23:50:07 -0500

    venv

---
+++ server.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+# XXX: The Database URI should be in the format of:
+#
+#     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/<DB_NAME>
+#
+# For example, if you had username ewu2493, password foobar, then the following line would be:
+#
+#     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
+#
+# For your convenience, we already set it to the class database
+
+# Use the DB credentials you received by e-mail
+DB_USER = "YOUR_DB_USERNAME_HERE"
+DB_PASSWORD = "YOUR_DB_PASSWORD_HERE"
+
+DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+
+DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
